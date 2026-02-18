b4c73cf59b272147b923fb9664192cc99cd8cf63
KuuKass <LauraJessica.Kuusma@tptlive.ee>
2026-02-19 00:10:53 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 async function getData() {
   const response = await fetch('https://official-joke-api.appspot.com/random_joke');
   const data = await response.json();
+  document.querySelector('#jokeText').textContent =
+    data.setup + " " + data.punchline;
   console.log(data);
 };
 const jokeButton = document.querySelector('#jokeBtn');
 jokeButton.addEventListener('click', getData);
+
